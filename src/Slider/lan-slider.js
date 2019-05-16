45f8bd3c2ff92a076fdf8fd13acafd356a8a0b56
EfunMa <elm91791@yahoo.com>
2019-05-16 15:45:59 -0700

    added progress bar

--- src/Slider/lan-slider.js
+++ src/Slider/lan-slider.js
@@ -50,6 +50,10 @@ export default class LANSlider extends HTMLElement {
     slider.onmouseout = () => (valueBox.style.cssText = "opacity: 0;");
 
     // Create some CSS to apply to the shadow dom
+    // const style = document.createElement("link");
+    // style.setAttribute('rel', 'stylesheet');
+    // style.setAttribute('type', 'text/css');
+    // style.setAttribute('href', './style.css');
     const style = document.createElement("style");
 
     style.textContent = `
@@ -66,78 +70,198 @@ export default class LANSlider extends HTMLElement {
         top: 5%;
         opacity: 0;
       }
-
       #lan-slider {
         -webkit-appearance: none;
         width: 100%;
         height: 7px;
         background: #409eff;
         border-radius: 10px;
+        border: none;
       }
       
       #lan-slider:focus {
         outline: none;
       }
       
-      #lan-slider::-ms-track {
-        width: 100%;
-        cursor: pointer;
-        background: transparent; 
-        border-color: transparent;
-        color: transparent;
-      }
+      // #lan-slider::-ms-track {
+      //   width: 100%;
+      //   cursor: pointer;
+      //   background: transparent; 
+      //   border-color: transparent;
+      //   color: transparent;
+      // }
+      
+      // #lan-slider::-moz-range-thumb {
+      //   border: 2px solid #409eff;
+      //   height: 25px;
+      //   width: 25px;
+      //   border-radius: 3px;
+      //   background: #ffffff;
+      //   cursor: pointer;
+      //   -moz-transition: 0.2s;
+      // }
+      // #lan-slider::-ms-thumb {
+      //   border: 2px solid #409eff;
+      //   height: 25px;
+      //   width: 25px;
+      //   border-radius: 3px;
+      //   background: #ffffff;
+      //   cursor: pointer;
+      //   transition: 0.2s;
+      // }
+      // #lan-slider::-webkit-slider-thumb:hover {
+      //   height: 30px;
+      //   width: 30px;
+      // }
+      // #lan-slider::-moz-range-thumb:hover {
+      //   height: 30px;
+      //   width: 30px;
+      // }
+      // #lan-slider::-ms-thumb:hover {
+      //   height: 30px;
+      //   width: 30px;
+      // }
 
-      #lan-slider::-webkit-slider-thumb {
+      .barCnt {
+        position: relative;
+        height: 10px;
+        padding: 5px 0px;
+        z-index: 200;
+      }
+      
+      .preBar{
+        position: relative;
+        z-index: 301;
+        width: 0;
+      }
+      
+      .barCnt .preBar {
+        /* position: absolute; */
+        background-color: #489fee;
+        height: 7px;
+        line-height: 6px;
+        /* z-index: 200; */
+        border-radius: 3px;
+        padding: 0px;
+        margin: 0px;
+        pointer-events: none;
+      }
+      
+      input[type=range].colorized {
         -webkit-appearance: none;
-        border: 2px solid #409eff;
-        height: 25px;
-        width: 25px;
-        border-radius: 100px;
-        background: #ffffff;
-        cursor: pointer;
-        -webkit-transition: 0.2s;
+        width: 100%;
+        height: 5px;
+        position: absolute;
+        padding: 0px;
+        margin: 0px;
+        cursor: ew-resize;
+        left: 0px
       }
-
-      #lan-slider::-moz-range-thumb {
-        border: 2px solid #409eff;
-        height: 25px;
-        width: 25px;
+      
+      input[type=range].colorized::-webkit-slider-runnable-track {
+        height: 7px;
+        background: lightgrey;
+        border: none;
         border-radius: 3px;
-        background: #ffffff;
-        cursor: pointer;
-        -moz-transition: 0.2s;
       }
-
-      #lan-slider::-ms-thumb {
-        border: 2px solid #409eff;
-        height: 25px;
-        width: 25px;
+      
+      input[type=range].colorized::-webkit-slider-thumb {
+        -webkit-appearance: none;
+        border: 2px solid #00001E;
+        height: 16px;
+        width: 16px;
+        border-radius: 50%;
+        background: white;
+        border-color: #489FEE;
+        margin-top: -4px;
+        z-index: 400;
+        position: relative;
+        cursor: pointer;
+        /* transition: height .01s ease-in-out; */
+      }
+      
+      input[type=range].colorized:focus {
+        outline: none;
+      }
+      
+      input[type=range].colorized:focus::-webkit-slider-runnable-track {
+        background: #ccc;
+      }
+      
+      input[type=range].colorized::-moz-range-track {
+        width: 100%;
+        height: 5px;
+        background: lightgray;
+        border: none;
         border-radius: 3px;
-        background: #ffffff;
-        cursor: pointer;
-        transition: 0.2s;
       }
-
-      #lan-slider::-webkit-slider-thumb:hover {
-        height: 30px;
-        width: 30px;
+      
+      input[type=range].colorized::-moz-range-thumb {
+        border: none;
+        height: 16px;
+        width: 16px;
+        border-radius: 50%;
+        background: lightgray;
+        z-index: 400;
       }
-
-      #lan-slider::-moz-range-thumb:hover {
-        height: 30px;
-        width: 30px;
-      }
-
-      #lan-slider::-ms-thumb:hover {
-        height: 30px;
-        width: 30px;
-      }
+      
+      input[type=range].colorized::-moz-focus-outer {
+        border: 0;
+      }      
     `;
+
+
+
+    var input = slider;
+    var wrp = document.createElement('div'),
+      preBar = document.createElement('p'),
+      range = max - min,
+      getVal = function () {
+        var t = (1 * (parseInt(input.value, 10) - parseInt(input.min, 10)) / range);
+        return t * 100;
+      };
+    wrp.className = 'barCnt';
+    preBar.className = 'preBar';
+
+    input.className = input.className.length ? (input.className + ' colorized') : 'colorized';
+    //input.parentNode.replaceChild(wrp, input);
+
+    wrp.appendChild(input);
+    wrp.appendChild(preBar);
+
+    //shadow.querySelector(".barCnt").appendChild(valueBox.cloneNode(true));
+
+    preBar.style.width = getVal() + '%';
+
+    var box = document.createElement('textarea');
+    box.innerText = parseInt(input.value, 10);
+    box.class = 'test';
+    shadow.appendChild(box.cloneNode(true));
+
+    // Change width of preBar depending on input
+    input.addEventListener('input', function () {
+      shadow.querySelector('textarea').value = shadow.querySelector('input').value;
+      preBar.style.width = getVal() + '%';
+
+      const valueBox = shadow.querySelector('#val-box');
+      console.log(valueBox.innerHTML);
+      console.log(shadow.querySelector('input').value);
+      valueBox.innerHTML = shadow.querySelector('input').value;
+      valueBox.style.cssText = `
+        opacity: 1;
+        left: ${calculatePosition(input.value, input.max, input.min)}%;
+        border-top: -100px;
+      `;
+      console.log("Adad" + valueBox.innerHTML);
+
+    });
+
 
     // Attach the created elements to the shadow dom
     shadow.appendChild(style);
-    shadow.appendChild(slider);
-    shadow.appendChild(valueBox);
+    //shadow.appendChild(slider);
+    shadow.appendChild(wrp);
+    shadow.querySelector('.barCnt').appendChild(valueBox);
   }
 
   /**
