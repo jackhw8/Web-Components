e050787168f5f8df3287d70c4851a5f888ab2876
Albert Putra Purnama <albertputrapurnama@gmail.com>
2019-06-12 00:49:33 -0700

    Integrated button bootstrap

--- packages/lan-button/src/main.js
+++ packages/lan-button/src/main.js
@@ -4,9 +4,9 @@ import { types, sizes } from "./constants.js";
 // Template of the actual button
 const template = document.createElement("template");
 template.innerHTML = `
-    <style>${componentStyle}</style>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <button type="button" class="button default"><slot name="button-label">Button</slot></button>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <style>${componentStyle}</style>
+  <button type="button" class="button default"></button>
 `;
 
 /**
@@ -23,18 +23,16 @@ export default class LANButton extends HTMLElement {
     // Create a shadow root
     const shadow = this.attachShadow({ mode: "open" });
 
-    // Create input button with default values
     const buttonComponent = template.content;
+    // Create input button with default values
     const text = this.innerHTML;
-    buttonComponent.querySelector("slot").innerHTML = text;
+    buttonComponent.querySelector('button').innerHTML = text;
 
     // Attach the created elements to the shadow dom
     shadow.appendChild(buttonComponent.cloneNode(true));
 
-    // set the class to default
-    shadow.querySelector("button").setAttribute("class", "button default");
-
-    this.updateButtonValue();
+    // check if bootstrap is enabled
+    this._checkbootstrap(shadow);
   }
 
   /**
@@ -45,6 +43,16 @@ export default class LANButton extends HTMLElement {
   attributeChangedCallback() {
     // update the value of the buttons
     this.updateButtonValue();
+  }
+
+  
+  /**
+   * connectedCallback invoked when the element is removed, appended, edited on
+   * the DOM tree.
+   */
+  connectedCallback() {
+    const root = this.shadowRoot;
+    this._checkbootstrap(root);
   }
 
   /**
@@ -60,8 +68,29 @@ export default class LANButton extends HTMLElement {
       "round",
       "autofocus",
       "disabled",
-      "class"
+      "bootstrap",
+      "className"
     ];
+  }
+
+  _checkbootstrap(shadow) {
+    // If bootstrap is enabled then reset the style etc.
+    if(this.hasAttribute("bootstrap")) {
+      // remove styles
+      shadow.querySelector('style').innerHTML = "";
+
+      // add class names to buttons
+      let classNames = this.getAttribute('className');
+      shadow.querySelector("button").setAttribute("class", classNames);
+    } else {
+      // remove link tags
+      shadow.removeChild(shadow.querySelector('link'));
+
+      // set the class to default
+      shadow.querySelector("button").setAttribute("class", "button default");
+
+      this.updateButtonValue();
+    }
   }
 
   /**
@@ -72,7 +101,7 @@ export default class LANButton extends HTMLElement {
     const buttonInput = shadow.querySelector("button");
 
     // Set native type of button
-    buttonInput.setAttribute("type", this.getAttribute("native-type"));
+    buttonInput.setAttribute("type", this.getAttribute("native-type") ? this.getAttribute("native-type"): "button");
     if (this.hasAttribute("autofocus")) {
       buttonInput.setAttribute("autofocus", "");
     }
@@ -82,17 +111,6 @@ export default class LANButton extends HTMLElement {
     this.updateButtonShape();
     this.updateButtonSize();
     this.updateDisabled();
-
-    // Integrate with bootstrap
-    if (this.hasAttribute("class")) {
-      if (this.getAttribute("class").includes("btn")) {
-        buttonInput.setAttribute(
-          "class",
-          buttonInput.getAttribute("class") + " " + this.getAttribute("class")
-        );
-        this.setAttribute("class", "");
-      }
-    }
   }
 
   /**
